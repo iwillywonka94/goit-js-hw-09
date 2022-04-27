febb1b781696b8ac167205bebe4de7d3ea71b6f9
iwillywonka94 <iwillywonka94@gmail.com>
2022-04-27 13:28:03 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+const body = document.querySelector("body")
+const buttonStart = document.querySelector("button[data-start]")
+const buttonEnd = document.querySelector("button[data-close]")
 
+console.log(body)
+console.log(buttonStart)
+console.log(buttonEnd)
